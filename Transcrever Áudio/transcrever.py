f58105cdc93f825c74cf45fe182af459baf704f5
Gustavo Receputi <receputi27dev@gmail.com>
2026-07-18 19:29:33 -0300

    Transcrição de Áudio
    Transcreve um audio ou video para TXT no terminal

---
+++ Transcrever Áudio/transcrever.py
@@ -0,0 +1,22 @@
+import whisper
+from tqdm import tqdm
+from pathlib import Path
+
+SCRIPT_DIR = Path(__file__).resolve().parent
+VIDEO_PATH = str(SCRIPT_DIR / "teste.mp4")
+OUTPUT_TXT = str(SCRIPT_DIR / "transcricao.txt")
+MODEL_NAME = "base"
+
+model = whisper.load_model(MODEL_NAME)
+
+print(f"Transcrevendo {VIDEO_PATH} com o modelo {MODEL_NAME}... \n")
+
+result = model.transcribe(
+    VIDEO_PATH, 
+    verbose=True,
+    fp16=False, 
+    word_timestamps=False
+)
+
+print("\n [TRANSCRIÇÃO COMPLETA]\n")
+print(result["text"])
