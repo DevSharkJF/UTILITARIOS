3bd277836a18310ef2a6c799721c3bdb1885de04
Gustavo Receputi <receputi27dev@gmail.com>
2026-07-20 11:43:24 -0300

    Transcrição TXT
    Salva a transcrição em um arquivo TXT por partes

--- Transcrever Áudio/transcrever.py
+++ Transcrever Áudio/transcrever.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 from pathlib import Path
 
 SCRIPT_DIR = Path(__file__).resolve().parent
-VIDEO_PATH = str(SCRIPT_DIR / "arquivo.extensao")
+VIDEO_PATH = str(SCRIPT_DIR / "teste.mp4")
 OUTPUT_TXT = str(SCRIPT_DIR / "transcricao.txt")
 MODEL_NAME = "base"
 
@@ -18,5 +18,15 @@ result = model.transcribe(
     word_timestamps=False
 )
 
-print("\n [TRANSCRIÇÃO COMPLETA]\n")
-print(result["text"])
+# Transcrição Completa pelo Terminal
+# print("\n [TRANSCRIÇÃO COMPLETA]\n")
+# print(result["text"])
+
+# Transcrição Completa por partes e salvando em um arquivo TXT
+with open(OUTPUT_TXT, "w", encoding="utf-8") as f:
+    for seg in tqdm (result["segments"], desc="Salvando transcrição"):
+        start = seg["start"]
+        end = seg["end"]
+        text = seg["text"].strip()
+        f.write(f"[{start:.2f} --> {end:.2f}] {text}\n")
+        print(f"\n[INFO] Transcrição salva em '{OUTPUT_TXT}'")
