25fd4dacf3d13f38640ac8455fc20efc8f5f920d
Gustavo Receputi <receputi27dev@gmail.com>
2026-08-12 17:55:06 -0300

    Remoção Vídeo
    O código de download de vídeo não baixa acima de 720p

--- PyTube Audio/audio.py
+++ PyTube Audio/audio.py
@@ -6,11 +6,11 @@ os.system('cls')
 link = input("🔗 Digite o link do Vídeo: ")
 yt = YouTube(link)
 
-destino = "./Download Audio/Áudios Baixados"
+destino = "./Áudios Baixados"
 audio = yt.streams.filter(only_audio=True, file_extension="mp4", mime_type="audio/mp4").first()
 
 print(f"⌛ Baixando áudio: {audio.title}")
-audio_file = audio.download(filename=f"{audio.title}audio.mp4", output_path=destino)
+audio_file = audio.download(filename=f"{audio.title}.mp4", output_path=destino)
 
 time.sleep(1.5)
 os.system('cls')
